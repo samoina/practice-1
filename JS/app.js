160e61b164fa1c6816dab2988b51f88a0cf0c86d
Samoina <samoina.w@gmail.com>
2022-01-24 10:22:09 +0300

    Add more practice tasks

--- JS/app.js
+++ JS/app.js
@@ -50,19 +50,87 @@ console.log(factorialize(20));
 function findLongestWordLength(str) {
   let stringSent = str
     .split(" ")
-    .sort(function(a,b) {
-      return b.length-a.length
+    .sort(function (a, b) {
+      return b.length - a.length
     })
 
-  return stringSent[0].length;
+  return stringSent[0];
 }
 
 console.log(findLongestWordLength("What if we try a super-long word such as otorhinolaryngology"));
 
 //<---------Return largest number in arrays---------->
 function largestOfFour(arr) {
-  return arr;
+  let sortArr = [];
+  // for (let index = 0; index < arr.length; index++) {
+  //   arr[index].sort(function (a, b) {
+  //     return b - a
+  //   })
+  //   sortArr.push(arr[index][0]);
+  // }
+  // arr.map(function(numbArr){
+  //   numbArr.sort(function(a,b) {
+  //     return b-a
+  //   })
+  //   sortArr.push(numbArr[0]);
+  // })
+  for (let index = 0; index < arr.length; index++) {
+    arr[index].sort(function (a, b) {
+      return b - a
+    })
+    sortArr.push(arr[index][0]);
+  }
+  return sortArr;
 }
 
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+
+// <----------Confirm the Ending-------->
+function confirmEnding(str, target) {
+//find length of target, slice string to -ve that length, then compare the two and if they are same, return true
+  let tarLength = target.length;
+  console.log(tarLength);
+
+  let checkStr = str.slice(-(tarLength));
+
+  if (checkStr === target) {
+    return true
+  } else {
+    return false
+  }
+}
+
+console.log(confirmEnding("Abstraction", "action"));
+
+
+//<------------Repeat a String Repeat a String---------->
+function repeatStringNumTimes(str, num) {
+  let strHolder="";
+  for (let index = 0; index <num; index++) {
+    strHolder= strHolder+str;
+  }
+  return strHolder
+}
+
+console.log(repeatStringNumTimes("abc", 4));
+
+
+//<-----------Truncate a String----------->
+function truncateString(str, num) {
+  let truncString = "";
+  if (str.length>num) {
+    truncString=str.slice(0, num).concat("...");
+    return truncString;
+  } else {
+    return str
+  }  
+}
+
+console.log(truncateString("Absolutely Longer", 2));
+
+
+
+//<----------Finders Keepers------------>
+function findElement(arr) {
+
 

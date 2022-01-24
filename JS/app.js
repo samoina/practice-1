f320e0d3aff4b882bd63f5f95a2656c4cfc6a760
Samoina <samoina.w@gmail.com>
2022-01-24 11:46:08 +0300

    Add few changes

--- JS/app.js
+++ JS/app.js
@@ -61,8 +61,12 @@ console.log(findLongestWordLength("What if we try a super-long word such as otor
 
 //<---------Return largest number in arrays---------->
 function largestOfFour(arr) {
+  
+
   return arr;
 }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+
 
